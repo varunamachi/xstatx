5f0978c73e65820d56f4d7786542fdf40a3c59e5
varunamachi <varunamachi@gmail.com>
2017-03-01 20:02:45 +0530

    Added logic for calculating CPU usage

--- CPUStatProvider.cpp
+++ CPUStatProvider.cpp
@@ -1,9 +1,6 @@
 #include <mutex>
 
-#if defined( Q_OS_LINUX )
-    #include <sys/utsname.h>
-#elif defined (Q_OS_WINDOWS)
-#endif
+#include <sys/utsname.h>
 
 #include <QThread>
 #include <QVector>
@@ -20,19 +17,35 @@
 
 namespace XStatx {
 
+struct CPUUsage
+{
+    CPUUsage()
+        : m_used{ 0 }
+        , m_idle{ 0 }
+        , m_total{ 0 }
+    {
+    }
+
+    double m_used;
+
+    double m_idle;
+
+    double m_total;
+};
+
 struct CPUStatProvider::Data
 {
     QPointer< CPUInfo > m_cpuInfo;
 
     double m_avgTemp;
 
-    double m_avgCPUUsage;
+    CPUUsage m_avgUsage;
 
     double m_avgFrequency;
 
     QVector< double > m_coreTemps;
 
-    QVector< double > m_coreUsage;
+    QVector< CPUUsage > m_coreUsage;
 
     QVector< double > m_coreFrequencies;
 
@@ -61,7 +74,7 @@ static CPUInfo::Architecture getArch()
 #ifdef Q_OS_LINUX
     utsname vla;
     uname( &vla );
-    QString archStr{ vla };
+    QString archStr{ vla.machine };
     if( archStr == "x86_64" ) {
         arch = CPUInfo::Architecture::x64;
     }
@@ -87,14 +100,14 @@ const CPUInfo * CPUStatProvider::getCPUInfo()
 
 double CPUStatProvider::getCPUUsage() const
 {
-    return m_data->m_avgCPUUsage;
+    return m_data->m_avgUsage.m_used;
 }
 
 double CPUStatProvider::getCPUUsage( std::uint8_t coreIndex ) const
 {
     LOCK();
     if( coreIndex < m_data->m_coreUsage.size() ) {
-        return m_data->m_coreUsage[ coreIndex ];
+        return m_data->m_coreUsage[ coreIndex ].m_used;
     }
     return 0;
 }
@@ -171,6 +184,7 @@ void CPUStatProvider::Data::updateFrequency()
     double cumulated = 0.0;
     if( cpuIDFile.exists() && cpuIDFile.open( QFile::ReadOnly )) {
         QTextStream reader{ &cpuIDFile };
+        auto coreIndex = 0;
         while( ! reader.atEnd() ) {
             auto line = reader.readLine();
             auto comps = line.split( ":" );
@@ -180,7 +194,12 @@ void CPUStatProvider::Data::updateFrequency()
                 if( key == "cpu MHz" ) {
                     std::lock_guard< std::mutex > guard( m_lock );
                     auto freq = value.toDouble();
-                    m_coreFrequencies.push_back( freq );
+                    if( coreIndex < m_coreFrequencies.size() ) {
+                        m_coreFrequencies[ coreIndex ] = freq;
+                    }
+                    else {
+                        m_coreFrequencies.push_back( freq );
+                    }
                     cumulated += freq;
                 }
             }
@@ -197,6 +216,72 @@ void CPUStatProvider::Data::updateFrequency()
 void CPUStatProvider::Data::updateTemparature()
 {
     std::lock_guard< std::mutex > guard( m_lock );
+}
+
+void CPUStatProvider::Data::updateCPUUsage()
+{
+//    from: http://stackoverflow.com/a/28063368
+//    user nice system idle iowait irq softirq  steal  guest  guest_nice
+//    Idle = idle + iowait
+//    NonIdle=user+nice+system+irq+softirq+steal
+//    Total=Idle+NonIdle # first line of file for all cpus
+//    CPU_Percentage=((Total-PrevTotal)-(Idle-PrevIdle))/(Total-PrevTotal)
+
+    auto calc = [ = ]( int coreIndex, QString &line ) {
+//        auto comps = line.split( "")
+        QTextStream stream{ &line };
+        double user = 0.0;
+        double nice = 0.0;
+        double system = 0.0;
+        double idle = 0.0;
+        double iowait = 0.0;
+        double irq = 0.0;
+        double softirq = 0.0;
+        double steal = 0.0;
+        stream  >> user
+                >> nice
+                >> system
+                >> idle
+                >> iowait
+                >> irq
+                >> softirq
+                >> steal;
+        auto nonIdle = user + nice + system + irq + softirq + steal;
+        auto totalIdle = idle + iowait;
+        auto total = idle + nonIdle;
+        auto &cur= m_avgUsage;
+        if( coreIndex != -1 && coreIndex < m_coreUsage.size() ) {
+            if( coreIndex == m_coreUsage.size() ) {
+                m_coreUsage.push_back( CPUUsage{ });
+            }
+            cur = m_coreUsage[ coreIndex ];
+        }
+        auto pc = (( total - cur.m_total ) - ( totalIdle - cur.m_idle ))
+                / ( total - cur.m_total );
+        cur.m_used = pc;
+        cur.m_total = total;
+        cur.m_idle = totalIdle;
+    };
+    QFile statFile{ "/proc/stat" };
+    auto isFirst = true;
+    auto coreIndex = 0;
+    if( statFile.exists() ) {
+        QTextStream stream{ &statFile };
+        while( ! stream.atEnd() ) {
+            auto line = stream.readLine();
+            if( isFirst && line.startsWith( "cpu" )) {
+                calc( -1, line );
+                isFirst = false;
+            }
+            else if( line.startsWith( "cpu" )) {
+                calc( coreIndex, line );
+                ++ coreIndex;
+            }
+            else {
+                break;
+            }
+        }
+    }
 }
 
 void CPUStatProvider::Data::refresh()
